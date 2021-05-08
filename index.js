c9df06d0b38ddd3d662fa97ef10fc5c7844339d7
Alexander Pitkin <peleccom@gmail.com>
2021-05-09 02:38:21 +0300

    Add arg parser

--- index.js
+++ index.js
@@ -1,60 +1,73 @@
 const puppeteer = require('puppeteer');
 const notifier = require('node-notifier');
-const trainConfig = require('./train_config');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 
 const config = {
-  trainConfig,
   headless: true, // Запуск в режиме браузера false
   selectors: {
-    table: '.sch-table__row-wrap'
-  }
+    table: '.sch-table__row-wrap',
+  },
 };
 
 let ticketsFound = false;
 let message = '';
 let trainFound = false;
 
-var startParser = async () => {
+function formatDate(d) {
+  return `${d.getFullYear()}-${d.getDate()}-${d.getMonth() + 1}`;
+}
+
+const startParser = async (trainConfig) => {
+  console.log(trainConfig);
   const browser = await puppeteer.launch({ headless: config.headless });
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768, deviceScaleFactor: 2 });
-  await page.goto(`https://pass.rw.by/ru/route/?from=${config.trainConfig.from}&to=${config.trainConfig.to}&date=${config.trainConfig.date}`);
+  await page.goto(
+    `https://pass.rw.by/ru/route/?from=${trainConfig.from}&to=${trainConfig.to}&date=${formatDate(
+      trainConfig.date,
+    )}`,
+  );
 
   // const trainCount = await page.$$eval(config.selectors.table + ' > tbody > tr', table => table.length);
   // console.log('Найдено поездов: ', trainCount || 0);
 
   const checkTrain = async () => {
-    const train = await page.$$eval(config.selectors.table,
-      (trainRow, trainNumber) => trainRow.reduce((result, item) => {
-        if (item.querySelector('.train-number').innerText.indexOf(trainNumber) !== -1) {
-          result.name = item.querySelector('.train-route').innerText;
-          places = []
-          item.querySelectorAll('.sch-table__t-item').forEach((el) => {
-            places.push({
-              type: item.querySelector('.sch-table__t-name').innerText,
-              tickets: item.querySelector('.sch-table__t-quant > span').innerText,
-            })
-          })
-          result.places = places
-        }
-        return result;
-      }, {}), config.trainConfig.trainNumber);
+    const train = await page.$$eval(
+      config.selectors.table,
+      (trainRow, trainNumber) =>
+        trainRow.reduce((result, item) => {
+          if (item.querySelector('.train-number').innerText.indexOf(trainNumber) !== -1) {
+            result.name = item.querySelector('.train-route').innerText;
+            places = [];
+            item.querySelectorAll('.sch-table__t-item').forEach((el) => {
+              places.push({
+                type: item.querySelector('.sch-table__t-name').innerText,
+                tickets: item.querySelector('.sch-table__t-quant > span').innerText,
+              });
+            });
+            result.places = places;
+          }
+          return result;
+        }, {}),
+      trainConfig.trainNumber,
+    );
 
     if (train.name) {
       const places = train.places.map((item) => {
-        const tickets = parseInt(item.tickets) | 0
-        const type = item.type
+        const tickets = parseInt(item.tickets) | 0;
+        const type = item.type;
         return {
           type,
           tickets,
-        }
+        };
       });
-      ticketsFound = places.some(item => item.tickets >= config.trainConfig.ticketCount);
+      ticketsFound = places.some((item) => item.tickets >= trainConfig.ticketCount);
       trainFound = true;
       message = places.reduce((message, item) => message + `${item.type}: ${item.tickets} `, '');
       console.log('check train');
     } else {
-      console.log('Поезд не найден ' + config.trainConfig.trainNumber);
+      console.log('Поезд не найден ' + trainConfig.trainNumber);
       await browser.close();
     }
   };
@@ -63,7 +76,7 @@ var startParser = async () => {
 
   const ticketFound = () => {
     notifier.notify({
-      title: 'Билеты найдены на поезд ' + config.trainConfig.trainNumber,
+      title: 'Билеты найдены на поезд ' + trainConfig.trainNumber,
       message,
       sound: true,
     });
@@ -84,12 +97,71 @@ var startParser = async () => {
     ticketFound();
     await browser.close();
   }
-
 };
 
-if (config.trainConfig.from.length && config.trainConfig.to.length && config.trainConfig.date && config.trainConfig.trainNumber.length) {
-  startParser();
-} else {
-  console.log('Введите номер поезда, станцию отправления, станцию назначения и дату.')
-}
+const argv = yargs(hideBin(process.argv))
+  .usage('Usage: $0 -f [from] -t [to] -d [date] -n [trainNumber] -t [count]')
+  .example('$0 -f Минск -t Брест -d 2020-05-08 -n 704Б -t 1')
+  .option('f', {
+    alias: 'from',
+    describe: 'Наименование станции отправления, пример: МИНСК-ПАССАЖИРСКИЙ',
+    type: 'string',
+  })
+  .option('t', {
+    alias: 'to',
+    describe: 'Наименование станции прибытия, пример: БРЕСТ-ЦЕНТРАЛЬНЫЙ',
+    type: 'string',
+  })
+  .option('d', {
+    alias: 'date',
+    describe: 'Дата поездки, пример: 2018-05-08, сегодня, завтра',
+    type: 'string',
+    coerce: (arg) => {
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      if (arg === 'сегодня') {
+        return today;
+      }
+      if (arg === 'завтра') {
+        const tomorrow = new Date(today);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        return tomorrow;
+      }
+      const date = new Date(arg);
+      if (isNaN(date) || date < today) {
+        throw Error(`Неверная дата: ${arg}`);
+      }
+      return date;
+    },
+  })
+  .option('n', {
+    alias: 'number',
+    describe: 'Номер поезда, пример: 607Б',
+    type: 'string',
+  })
+  .option('c', {
+    alias: 'count',
+    describe: 'Количество билетов',
+    type: 'number',
+    coerce: (count) => {
+      if (count === undefined) {
+        return 1;
+      }
+      if (isNaN(count) || count < 1) {
+        throw Error('неверное количесто мест');
+      }
+      return count;
+    },
+  })
+  .demandOption(
+    ['f', 't', 'd', 'n', 'c'],
+    'Введите номер поезда, станцию отправления, станцию назначения и дату',
+  ).argv;
 
+startParser({
+  from: argv.from,
+  to: argv.to,
+  date: argv.date,
+  trainNumber: argv.number,
+  ticketCount: argv.count,
+});
