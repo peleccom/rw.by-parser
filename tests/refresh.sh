36c72ff3f30897d815da04c8b9b05970a08cee14
Alexander Pitkin <peleccom@gmail.com>
2022-07-06 09:36:02 +0300

    Add refresh script

---
+++ tests/refresh.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+wget -O test.html https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%9F%D0%B0%D1%81%D1%81%D0%B0%D0%B6%D0%B8%D1%80%D1%81%D0%BA%D0%B8%D0%B9&to=%D0%92%D0%B8%D1%82%D0%B5%D0%B1%D1%81%D0%BA
